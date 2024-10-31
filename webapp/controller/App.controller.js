3fce191bd342e12f37bd167aba4f14e2e9891ab0
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2024-10-31 01:17:46 +0000

    Envio Inicial App

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("student28.sap.training.diagram.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
